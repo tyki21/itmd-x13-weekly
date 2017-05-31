facf8d5d3bb0d3c367cc67bb9ff27271db614549
Tykiera <tolivest@hawk.iit.edu>
2017-05-31 18:24:10 -0500

    Finish exercises

--- pine/chap04/variable.rb
+++ pine/chap04/variable.rb
@@ -1,1 +1,20 @@
-##first example
+#first example
+puts '... you can say that again ...'
+
+my_string = '... you can say that again ...'
+
+puts my_string
+puts my_string
+
+name = 'Tykiera Mahogany OliveStalling'
+
+puts 'My name is ' + name + '.'
+
+puts "  "
+#Like java they variables in Ruby can hold anything
+
+string_var = 'Just another string'
+puts string_var
+
+number_var = 5 * 12
+puts number_var
