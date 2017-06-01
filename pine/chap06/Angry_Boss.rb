731d88c119a15302541f827a3a46aa914bc8a0ed
Tykiera <tolivest@hawk.iit.edu>
2017-05-31 23:32:39 -0500

    Add Few things to try Angry Boss

---
+++ pine/chap06/Angry_Boss.rb
@@ -0,0 +1,5 @@
+puts "You are going to tell your boss a very important questiont today... so go ahead and type it in: "
+
+question_up = gets.chomp
+
+puts "Your boss says : WHADDAYA MEAN \"" + question_up.upcase + "\"?!? YOU\'RE FIRED!!"
