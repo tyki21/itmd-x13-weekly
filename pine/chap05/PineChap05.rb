852b62ecc48a8bb92006c530687b9d461394fb44
Tykiera <tolivest@hawk.iit.edu>
2017-05-31 19:34:59 -0500

    Add a few things to try

---
+++ pine/chap05/PineChap05.rb
@@ -0,0 +1,18 @@
+##Few things to try
+puts "Please enter your first name"
+fname = gets.chomp
+puts "Please enter your middle name"
+mname = gets.chomp
+puts "Please enter your last name"
+lname = gets.chomp
+
+puts "Hi nice to meet you " + fname + " " + mname + " " + lname + "."
+
+#Fav number
+puts " "
+
+puts "What\'s your favorite number?"
+num = gets.chomp
+puts " "
+
+puts "I see that " + num.to_i + " is your favorite number"
