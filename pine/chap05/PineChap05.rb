1023a5334d1c6df1117f2658b60b702a31f80f1d
Tykiera <tolivest@hawk.iit.edu>
2017-05-31 20:00:50 -0500

    Finish the few things to try

--- pine/chap05/PineChap05.rb
+++ pine/chap05/PineChap05.rb
@@ -15,4 +15,7 @@ puts "What\'s your favorite number?"
 num = gets.chomp
 puts " "
 
-puts "I see that " + num.to_i + " is your favorite number"
+puts "I see that " + num + " is your favorite number"
+num2 = num.to_i + 1
+
+puts "However I would like let you know that " + (num2.to_s) + " is a sLiGhTy better number. It should definitely have a place in your heart as the new favorite."
