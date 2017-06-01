d94af7d5c21e323242cd97e371124f93a519b9db
Tykiera <tolivest@hawk.iit.edu>
2017-05-31 19:32:17 -0500

    Add more in book examples

--- pine/chap05/funny_strings.rb
+++ pine/chap05/funny_strings.rb
@@ -1,7 +1,21 @@
+var1 = 2
+var2 = '5'
+
+
+#wrong
+puts 'var1 + var2'
+
+#right
+puts var1.to_s + var2
+#or
+puts var1 + var2.to_i
+puts " "
+
 #Chomp method
 puts 'Hello there and what\'s your name?'
 name = gets.chomp
 puts 'Please to meet  you, ' + name +'. :P'
+
 
 
 ##puts gets
