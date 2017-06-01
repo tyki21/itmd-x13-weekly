54bee15c5c016117b064b16824e6f48ac2807658
Tykiera <tolivest@hawk.iit.edu>
2017-06-01 13:48:44 -0500

    Organize folders and files

---
+++ pine/A Few Things to try/99_Bottles_of_Beer.rb
@@ -0,0 +1,14 @@
+bottles = 99
+
+bottlesOfBeer = bottles.to_s
+
+
+    puts "Lets start singing!"
+    puts  bottlesOfBeer + " bottles of beer on the wall! " + bottlesOfBeer + " bottles of Beer!"
+while bottles < 0
+(bottles - 1)
+bottlesOfBeer = bottles.to_s
+
+    puts "You take one down, pass it around " + bottlesOfBeer + " bottles of Beer on the wall!"
+
+end
--- pine/chap07/99_Bottles_of_Beer.rb
+++
@@ -1,4 +0,0 @@
-bottlesOfBeer = 99
-
-puts "Lets start singing!"
-puts  bottlesOfBeer. ""
