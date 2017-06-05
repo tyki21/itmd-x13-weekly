2352dfb04b96b42646333d322d22980584173db2
Tykiera <tolivest@hawk.iit.edu>
2017-06-05 11:06:50 -0500

    deleting pine and demo

---
+++ pine/chap05-chap07/chap06/methods.rb
@@ -0,0 +1,19 @@
+puts self
+
+var1 = 'stop'
+var2 = 'deliver repaid desserts'
+
+puts var1.reverse
+puts var2.reverse
+
+puts var1
+puts var2
+
+puts " "
+
+puts 'What is your full name'
+name = gets.chomp
+
+puts 'did you know there are ' + name.length.to_s + ' characters in your name'
+
+puts 'You have a long name ' + name + '!'
