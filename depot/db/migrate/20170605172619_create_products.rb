04428511547d51afb151761a7dee95b4a3cbbc22
Tykiera <tolivest@hawk.iit.edu>
2017-06-05 14:14:53 -0500

    first step of task A

--- depot/db/migrate/20170605172619_create_products.rb
+++ depot/db/migrate/20170605172619_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
-      t.string : title
+      t.string :title
       t.text :description
       t.string :image_url
       t.decimal :price
