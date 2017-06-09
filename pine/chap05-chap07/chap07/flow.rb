724a424d1ae7c83dd59a8f1ff2e3500a542f17eb
Tykiera <tolivest@hawk.iit.edu>
2017-06-09 11:54:44 -0500

    More exercises

--- pine/chap05-chap07/chap07/flow.rb
+++ pine/chap05-chap07/chap07/flow.rb
@@ -13,3 +13,24 @@ puts 'cat' < 'dog'
 
 puts 'bug lady' < 'Xander'
 puts 'bug lady'.downcase < 'Xander'.downcase
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+
+puts 'Hello, ' + name + '.'
+
+if name == 'Chris'
+  puts 'what a boring name'
+end
+
+
+
+puts 'I am a fortune-teller. Tell me your name.'
+name = gets.chomp
+
+if name == 'TK'
+  puts 'I see great things in your future.'
+else
+  puts 'Your future is ... oh my! Look at the time!'
+  puts 'I really have to go, sorry!'
+end
