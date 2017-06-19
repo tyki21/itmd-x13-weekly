1c05b339f1e9c2f0f2dd389ad9f848dc9565a4ae
Tykiera <tolivest@hawk.iit.edu>
2017-06-19 14:36:01 -0500

    Adding some stylistic changes and setting up the routes

--- The_market/config/routes.rb
+++ The_market/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :carts
-  root 'store#index'
-
+  root 'store#index', as: 'store_index'
   resources :products
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
