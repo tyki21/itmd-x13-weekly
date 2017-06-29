c441e3e064b0502f3480929a58d028de2c1cd9fa
Tykiera <tolivest@hawk.iit.edu>
2017-06-28 19:35:38 -0500

    Add pictures and seed data

--- depot/db/seeds.rb
+++ depot/db/seeds.rb
@@ -14,10 +14,21 @@ description:
 <em>Native Apps, Multiple Platforms</em>
 Answer the question “Can we build this for ALL the devices?” with a
 resounding YES. This book will help you get there with a real-world
-introduction to seven platforms, whether you’re new to mobile or an 
+introduction to seven platforms, whether you’re new to mobile or an
 experienced developer needing to expand your options. Plus, you’ll find
 out which cross-platform solution makes the most sense for your needs.
 </p>},
 image_url: '7apps.jpg',
 price: 26.00)
 # . . .
+
+Product.create!(title: 'Ladies Love CoolJ',
+description:
+%{<p>
+  <em>Lots of Ladies, Only Cool J</em>
+
+  Tell me, do Ladies love CoolJ? I know I do and I wrote a book on it.
+  </p>},
+
+  image_url: 'll.jpg',
+  price: 15.00)
